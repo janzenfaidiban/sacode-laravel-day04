c091e415af38a118929cbacf915c5f68da85d95c
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-12-06 19:54:44 +0900

    MODIFIED: nav.php

--- template/views/partials/nav.php
+++ template/views/partials/nav.php
@@ -2,10 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <a href="?page=home" class="btn btn-dark">Home</a>
-                <a href="?page=profile" class="btn btn-dark">Profile</a>
-                <a href="?page=gallery" class="btn btn-dark">Gallery</a>
-                <a href="?page=contact" class="btn btn-dark">Contact</a>
+                <a href="?page=home" class="btn <?php if(isset($_GET['page']) && $_GET['page'] == 'home') { echo 'btn-dark'; } else { echo 'btn-outline-dark'; } ?>">Home</a>
+                <a href="?page=profile" class="btn <?php if(isset($_GET['page']) && $_GET['page'] == 'profile') { echo 'btn-dark'; } else { echo 'btn-outline-dark'; } ?>">Profile</a>
+                <a href="?page=gallery" class="btn <?php if(isset($_GET['page']) && $_GET['page'] == 'gallery') { echo 'btn-dark'; } else { echo 'btn-outline-dark'; } ?>">Gallery</a>
+                <a href="?page=contact" class="btn <?php if(isset($_GET['page']) && $_GET['page'] == 'contact') { echo 'btn-dark'; } else { echo 'btn-outline-dark'; } ?>">Contact</a>
             </div>
         </div>
     </div>
