8557a53aca3f519b98b1399d4883dc4c75dfa57b
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-12-06 19:08:24 +0900

    added: template folder

---
+++ template/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SaCode</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+    <!-- header start -->
+    <!-- header end -->
+    
+    <!-- nav start -->
+    <!-- nav end -->
+
+    <!-- main start -->
+    <!-- main end -->
+
+    <!-- footer start -->
+    <!-- footer end -->
+</body>
+</html>
