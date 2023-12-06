79cabf7bf03c2907932f016c85af4f0999365001
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-12-06 20:02:08 +0900

    added: settings, config

---
+++ template/config/settings.php
@@ -0,0 +1,9 @@
+<?php 
+
+    $siteTitle = "SaCode";
+    $siteDescription = "Papua Tech Community & Papua Coding School";
+    $siteFavicon = "img/fav-janzen.jpg";
+
+    // social medias
+    $instagram = "https://instagram.com/sacode.official";
+    $facebook = "https://facebook.com/";
--- template/index.php
+++ template/index.php
@@ -1,10 +1,14 @@
+<?php include('config/settings.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SaCode</title>
+    <title><?=$siteTitle?></title>
     <link rel="stylesheet" href="./css/style.css">
+
+    <link rel="icon" type="image/x-icon" href="<?=$siteFavicon?>">
+
 </head>
 <body>
 
