cb4c3bda8b3bac1394a1d777d736d22e6400e354
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-12-06 19:38:56 +0900

    added: page not found

--- template/index.php
+++ template/index.php
@@ -21,6 +21,19 @@
 
         if(isset($_GET['page']) && $_GET['page'] == 'home'){
             include('views/pages/home.php');
+        } else {
+            echo '
+            <section class="p-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <h3>Error 404</h3>
+                            <p>page not found!</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            ';
         }
 
     ?>
---
+++ template/views/pages/profile.php
@@ -0,0 +1,21 @@
+<section class="bg-dark text-light p-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 mx-auto text-center">
+                <h2>Profile</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            </div>
+        </div>
+        <!-- .row end -->
+
+        <div class="row">
+            <div class="col-12">
+                <h3>Our History</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat doloremque architecto ipsam veritatis sunt repellendus, iste deleniti eum at vero ea. Modi itaque nihil tempora.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat doloremque architecto ipsam veritatis sunt repellendus, iste deleniti eum at vero ea. Modi itaque nihil tempora.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat doloremque architecto ipsam veritatis sunt repellendus, iste deleniti eum at vero ea. Modi itaque nihil tempora.</p>
+            </div>
+        </div>
+        <!-- .row end -->
+    </div>
+</section>
